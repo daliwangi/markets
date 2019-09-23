cb35e18b45aa0e88433c8b074a897aad1c6b7182
JSN <39099063+mountaineerbr@users.noreply.github.com>
2019-09-23 19:15:44 -0300

    Add files via upload

--- pricesroll.sh
+++ pricesroll.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-uxterm -T 'BNB Stream' -geometry 6x29+0+24 -e '/home/jsn/_Scripts/markets/binance.sh -f3 -w bnbusdt' &
+uxterm -T 'BNB Stream' -geometry 6x25+0+340 -e '/home/jsn/_Scripts/markets/binance.sh -f3 -w bnbusdt' &
 
-uxterm -T 'LTC Stream' -geometry 6x24+0+0 -e '/home/jsn/_Scripts/markets/binance.sh -s ltcusdt' &
+uxterm -T 'LTC Stream' -geometry 6x25+54+340 -e '/home/jsn/_Scripts/markets/binance.sh -s ltcusdt' &
 
-uxterm -T 'BTC Bitstamp Stream' -geometry 8x29+52+24 -e "/home/jsn/_Scripts/markets/bitstamp.sh -w" &
+uxterm -T 'BTC Bitstamp Stream' -geometry 7x25+108+340 -e "/home/jsn/_Scripts/markets/bitstamp.sh -w" &
 
-uxterm -T 'XRP Binance Stream' -geometry 6x24+70+0 -e "/home/jsn/_Scripts/markets/binance -f3 -s xrpusdt" &
+uxterm -T 'XRP Binance Stream' -geometry 7x20+0+0 -e "/home/jsn/_Scripts/markets/binance.sh -f5 -s xrpusdt" &
 
-uxterm -T 'BTC Detailed Stream' -geometry 77x53+121+0 -e '/home/jsn/_Scripts/markets/binance.sh -i' &
+uxterm -T 'BCHABC Binance Stream' -geometry 6x20+62+0 -e "/home/jsn/_Scripts/markets/binance.sh -w bchabcusdt" &
 
-uxterm -T 'Book Depth' -geometry 36x44+741+0 -e '/home/jsn/_Scripts/markets/binance.sh -e' &
+uxterm -T 'ETH Binance Stream' -geometry 6x20+115+0 -e "/home/jsn/_Scripts/markets/binance.sh -s ethusdt" &
+
+uxterm -T 'BTC Detailed Stream' -geometry 77x53+172+0 -e '/home/jsn/_Scripts/markets/binance.sh -i' &
+
+uxterm -T 'Book Depth' -geometry 36x44+800+0 -e '/home/jsn/_Scripts/markets/binance.sh -e' &
 
 uxterm -T 'BTC New-Block' -geometry 74x18+506+487 -e '/home/jsn/_Scripts/markets/binfo.sh -e' &
 
