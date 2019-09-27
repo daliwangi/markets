fc951922c690284c07a5d7d8720ebeb1f4c7cf5d
JSN <39099063+mountaineerbr@users.noreply.github.com>
2019-09-27 17:45:14 -0300

    Add files via upload

--- parmetal.sh
+++ parmetal.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# v0.2.4  09/set/2019 by mountaineer_br
+# v0.2.6  27/set/2019 by mountaineer_br
 # Free Software under the GNU Public License 3
 
 LC_NUMERIC=en_US.UTF-8
 
 ## Taxas da PARMETAL
 # Ajuda "-h"
-[[ "${1}" = "-h" ]] &&
-	printf "Uso:\n\tparmetal\t#Todas cotações disponíveis\n" &&
-		printf "\tparmetal -p\t#Somente Barras Parmetal\n" &&
+if [[ "${1}" = "-h" ]]; then
+	printf "Uso:\n\tparmetal\t#Todas cotações disponíveis\n"
+	printf "\tparmetal -p\t#Somente Barras Parmetal\n"
 	exit 0
+fi
 
 # Removing all HTML tags from a webpage (for use with curl)
-htmlfilter() {
-	sed -E 's/<[^>]*>//g'
-}
+htmlfilter() { sed -E 's/<[^>]*>//g';}
 
 # Pegar taxas somente das Barras da Parmetal "-p"
 if [[ "${1}" = "-p" ]]; then
@@ -22,11 +21,10 @@ if [[ "${1}" = "-p" ]]; then
 	# From:https://unix.stackexchange.com/questions/277861/parse-xml-returned-from-curl-within-a-bash-script
 	PRICE="$(curl -s "https://www.parmetal.com.br/app/metais/" |
 		sed -E 's/<[^>]*>/]/g' | sed 's/]]]]/[[/g' |
-		sed 's/]]]/[/g' | sed 's/]]/[/g' |
-		sed 's/\[/\n/g' |
+		sed 's/]]]/[/g' | sed 's/]]/[/g' | sed 's/\[/\n/g' |
 		grep --color=never -A2 -e "Barra Parmetal/RBM")"
 	printf "%s\n" "${PRICE}"
-	SPREAD="$(printf "((%s/%s)-1)*100\n" "$(printf "%s\n" "${PRICE}" | tail -n 1)" "$(printf "%s\n" "${PRICE}" | tail -n 2 | head -n 1)" | tr ',' '.' | bc -l)"
+	SPREAD="$(bc -l "(($(tail -n 1 <<< "${PRICE}")/$(tail -n 2 <<< "${PRICE}" | head -n 1)" | tr ',' '.'))-1)*100\n" "" ""
 	printf "SPD: %'.3f %%\n" "${SPREAD}"
 	exit
 fi
@@ -36,25 +34,23 @@ metaisf() {
 	METAIS="$(curl -s "https://www.parmetal.com.br/app/metais/" |
 		sed -E 's/<[^>]*>/]/g' | sed 's/]]]]/[[/g' | sed 's/]]]/[/g' |
 		sed 's/]]/[/g' | sed 's/\[/\n/g')"
-	BPARM=( $(printf "%s\n" "${METAIS}" | grep -iA2 "barra parmetal" | sed 's/$/=/g') )
-	BTRAD=( $(printf "%s\n" "${METAIS}" | grep -iA2 "barras tradicionais" | sed 's/$/=/g') )
-	BOUTR=( $(printf "%s\n" "${METAIS}" | grep -iA2 "outras barras" | sed 's/$/=/g') )
-	UPDATES="$(printf "%s\n" "${METAIS}" | grep -i -e "../../...." | sort | uniq)"
-	UPTIMES="$(printf "%s\n" "${METAIS}" | grep -i -e "..:..:.." | sort | uniq)"
+	BPARM=( $(grep -iA2 "barra parmetal" <<< "${METAIS}" | sed 's/$/=/g') )
+	BTRAD=( $(grep -iA2 "barras tradicionais" <<< "${METAIS}" | sed 's/$/=/g') )
+	BOUTR=( $(grep -iA2 "outras barras" <<< "${METAIS}" | sed 's/$/=/g') )
+	UPDATES="$(grep -i -e "../../...." <<< "${METAIS}" | sort | uniq)"
+	UPTIMES="$(grep -i -e "..:..:.." <<< "${METAIS}" | sort | uniq)"
 	}
 
 # Moedas de Câmbios
 moedasf() {
 	MOEDAS="$(curl -s "https://www.parmetal.com.br/app/subtop-cotacao/" |
-	htmlfilter |
-	sed 's/&nbsp;//g' |
-	grep -i -e dolar -e libra -e "ouro spot" -e euro |
-	sed -e 's/^[ \t]*//' -e 's/Valor: //g' | tr '.' ',')" 
+		htmlfilter | sed 's/&nbsp;//g' | grep -i -e dolar -e libra -e "ouro spot" -e euro |
+		sed -e 's/^[ \t]*//' -e 's/Valor: //g' | tr '.' ',')" 
 	# Preparar para Tebela
-	USD=( $(printf "%s\n" "${MOEDAS}" | grep -i dolar | sed 's/cial:/cial=/g') )
-	GBP=( $(printf "%s\n" "${MOEDAS}" | grep -i libra | sed 's/cial:/cial=/g') )
-	XAU=( $(printf "%s\n" "${MOEDAS}" | grep -i ouro | sed 's/Spot:/Spot=/g') )
-	EUR=( $(printf "%s\n" "${MOEDAS}" | grep -i euro | sed 's/cial:/cial=/g') )
+	USD=( $(grep -i "dolar" <<< "${MOEDAS}" | sed 's/cial:/cial=/g') )
+	GBP=( $(grep -i "libra" <<< "${MOEDAS}" | sed 's/cial:/cial=/g') )
+	XAU=( $(grep -i "ouro" <<< "${MOEDAS}" | sed 's/Spot:/Spot=/g') )
+	EUR=( $(grep -i "euro" <<< "${MOEDAS}" | sed 's/cial:/cial=/g') )
 	}
 
 # Imprimir Tabela
@@ -66,7 +62,8 @@ printf "%s\n%s\n%s\n" "${BPARM[*]}" "${BTRAD[*]}" "${BOUTR[*]}" |
 # Spread
 #printf "%s\n" "${BPARM[2]%\=}" 
 #printf "%s\n" "${BPARM[3]%\=}"
-SPREAD="$(printf "((%s/%s)-1)*100\n" "$(printf "%s\n" "${BPARM[@]}" | tail -n 1)" "$(printf "%s\n" "${BPARM[@]}" | tail -n 2 | head -n 1)" | tr ',' '.' | tr -d '=' | bc -l)"
+#SPREAD="$(printf "((%s/%s)-1)*100\n" "$(printf "%s\n" "${BPARM[@]}" | tail -n 1)" "$(printf "%s\n" "${BPARM[@]}" | tail -n 2 | head -n 1)" | tr ',' '.' | tr -d '=' | bc -l)"
+SPREAD="$(tr ',' '.' <<< "(($(printf "%s\n" "${BPARM[@]}" | tail -n 1)/$(printf "%s\n" "${BPARM[@]}" | tail -n 2 | head -n 1))-1)*100" | tr -d '=' | bc -l)"
 printf " SPD B Parmetal/RBM     %'.3f%%\n" "${SPREAD}"
 # Update timestamps
 printf "Updates: %s %s\n\n" "${UPDATES}" "${UPTIMES}"
@@ -75,5 +72,4 @@ printf "Updates: %s %s\n\n" "${UPDATES}" "${UPTIMES}"
 moedasf
 printf "%s\n%s\n%s\n%s\n" "${USD[*]}" "${GBP[*]}" "${XAU[*]}" "${EUR[*]}" |
 	column -t -s"=" -N'              Índice,Taxa' -R'              Índice'
-#printf "Update: ??\n"
 
