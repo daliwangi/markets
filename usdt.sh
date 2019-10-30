46979a01a6fed272d9bcde1708c76981bfcd4d6d
mountaineerbr <39099063+mountaineerbr@users.noreply.github.com>
2019-10-30 12:03:12 -0300

    Add files via upload

--- usdt.sh
+++ usdt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# v0.3  05/10/2019  by mountaineer_br
+# v0.3.1  05/10/2019  by mountaineer_br
 
 # Check Tether rates
 LC_NUMERIC="en_US.UTF-8"
 printf "\nUSDT/USD Rates\n\n"
 
-CLIBJSON=$(curl -s "https://coinlib.io/api/v1/coin?key=fdcd5af7010ab1e0&pref=USD&symbol=USDT")
+CLIBJSON=$(curl -s "https://coinlib.io/api/v1/coin?key=${CLIBAPIKEY}&pref=USD&symbol=USDT")
 ENAMES=($(printf "%s\n" "${CLIBJSON}" | jq -r ".markets[0]|.exchanges[]|.name"))
 PRICES=($(printf "%s\n" "${CLIBJSON}" | jq -r ".markets[0]|.exchanges[]|.price"))
 
