e989642bfd8c6d806b927479125aa705bfbcbd5d
mountaineerbr <39099063+mountaineerbr@users.noreply.github.com>
2019-11-11 23:49:28 -0300

    Add files via upload

--- pricesroll.sh
+++ pricesroll.sh
@@ -4,13 +4,15 @@ uxterm -T 'BNB Stream' -geometry 6x25+0+340 -e '/home/jsn/_scripts/markets/binan
 
 uxterm -T 'LTC Stream' -geometry 6x25+54+340 -e '/home/jsn/_scripts/markets/binance.sh -s ltcusdt' &
 
-uxterm -T 'BTC Bitstamp Stream' -geometry 7x25+108+340 -e "/home/jsn/_scripts/markets/bitstamp.sh -w" &
+uxterm -T 'BTC Bitfinex Stream' -geometry 7x13+108+340 -e "/home/jsn/_scripts/markets/bitfinex.sh" &
+
+uxterm -T 'BTC Bitstamp Stream' -geometry 7x11+108+570 -e "/home/jsn/_scripts/markets/bitstamp.sh -c" &
 
 uxterm -T 'XRP Binance Stream' -geometry 7x20+0+0 -e "/home/jsn/_scripts/markets/binance.sh -f5 -s xrpusdt" &
 
 uxterm -T 'BCHABC Binance Stream' -geometry 6x20+62+0 -e "/home/jsn/_scripts/markets/binance.sh -w bchabcusdt" &
 
-uxterm -T 'ETH Binance Stream' -geometry 6x20+115+0 -e "/home/jsn/_scripts/markets/binance.sh -s ethusdt" &
+uxterm -T 'ETH Binance Stream' -geometry 6x20+115+0 -e "/home/jsn/_scripts/markets/binance.sh -w ethusdt" &
 
 uxterm -T 'BTC Detailed Stream' -geometry 77x53+172+0 -e '/home/jsn/_scripts/markets/binance.sh -i' &
 
@@ -19,37 +21,4 @@ uxterm -T 'Book Depth' -geometry 36x44+800+0 -e '/home/jsn/_scripts/markets/bina
 uxterm -T 'BTC New-Block' -geometry 74x18+506+487 -e '/home/jsn/_scripts/markets/binfo.sh -e' &
 
 uxterm -T 'XAU-XAG-BRL Price roll' -geometry 30x42+1149+149 &
-
-exit
-
-OLD CONFIGS
-
-
-xfce4-terminal -T 'BNB Stream' --geometry 6x43+0+0 -e '/home/jsn/_Scripts/markets/binance.sh -f3 -w bnbusdt' &
-
-xfce4-terminal -T 'BTC Bitstamp Stream' --geometry 8x43+52+0 -e "/home/jsn/_Scripts/markets/bitstamp.sh -w" &
-
-xfce4-terminal -T 'BTC Detailed Stream' --geometry 77x44+120+0 -e '/home/jsn/_Scripts/markets/binance.sh -i' &
-
-xfce4-terminal -T 'Book Depth' --geometry 36x43+740+0 -e '/home/jsn/_Scripts/markets/binance.sh -e' &
-
-xfce4-terminal -T 'BTC New-Block' --geometry 74x18+480+439 -e '/home/jsn/_Scripts/markets/binfo.sh -e' &
-
-xfce4-terminal -T 'XAU-XAG-BRL Price roll' --geometry 32x35+1106+149 &
-
-
-
-
-
-xfce4-terminal -T 'BTC Stream' --geometry 8x43+0+20 -e '/home/jsn/_Scripts/markets/binance.sh -s' &
-
-xfce4-terminal -T 'BTC Bitstamp Stream' --geometry 8x43+68+20 -e "/home/jsn/_Scripts/markets/bitstamp.sh -w" &
-
-xfce4-terminal -T 'BNB Stream' --geometry 6x43+136+20 -e '/home/jsn/_Scripts/markets/binance.sh -f3 -w bnbusdt' &
-
-xfce4-terminal -T 'Book Depth' --geometry 36x43+188+20 -e '/home/jsn/_Scripts/markets/binance.sh -e' &
-
-xfce4-terminal -T 'BTC New-Block' --geometry 74x18+480+439 -e '/home/jsn/_Scripts/markets/binfo.sh -e' &
-
-xfce4-terminal -T 'XAU-XAG-BRL Price roll' --geometry 37x35+1066+149 &
 
