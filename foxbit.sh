dbdf81812aee8dc7b39d11f56ad28e6b1bf2ad25
mountaineerbr <39099063+mountaineerbr@users.noreply.github.com>
2019-11-27 06:34:46 -0300

    Add files via upload

--- foxbit.sh
+++ foxbit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Foxbit.sh -- Pegar taxas de criptos pelo API da FoxBit
-# v0.2.26  27/nov/2019  by mountaineer_br
+# v0.2.27  27/nov/2019  by mountaineer_br
 
 HELP="GARANTIA
 	Este programa/script é software livre e está licenciado sob a Licença 
@@ -201,9 +201,10 @@ fi
 
 ## Price of Instrument
 statsf () {
-	printf "Ticker Rolante\n"
 	websocat ${KEEPCONN} -t --ping-interval 20 "wss://apifoxbitprodlb.alphapoint.com/WSGateway" <<< '{"m":0,"i":4,"n":"SubscribeTicker","o":"{\"OMSId\":1,\"InstrumentId\":'${ID}',\"Interval\":'${INTV}',\"IncludeLastCount\":1}"}' | jq --unbuffered -r '.o' |
-		jq --unbuffered -r --arg IDNA "${IDNAME}" '.[] | "InstrumentID: \(.[8]) (\($IDNA))",
+		jq --unbuffered -r --arg IDNA "${IDNAME}" '.[] |"",
+			"Foxbit Ticker Rolante",
+			"InstrumentID: \(.[8]) (\($IDNA))",
 			"Hora_Inicial: \((.[9]/1000) | strflocaltime("%Y-%m-%dT%H:%M:%S%Z"))",
 			"Hora_Final__: \((.[0]/1000) | strflocaltime("%Y-%m-%dT%H:%M:%S%Z"))",
 			"Intervalo___: \((.[0]-.[9])/1000) secs (\((.[0]-.[9])/3600000) h)",
