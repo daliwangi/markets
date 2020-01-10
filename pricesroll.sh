eddbd3b1dc31cefc9e9721491b28c0cd297cb815
castaway <39099063+mountaineerbr@users.noreply.github.com>
2020-01-10 12:16:56 -0300

    Add files via upload

--- pricesroll.sh
+++ pricesroll.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-uxterm -T 'BNB Binance' -geometry 6x25+0+340 -e '/home/jsn/bin/markets/binance.sh -af3 -w bnbusdt' &
+xfce4-terminal -T 'BNB Binance' --geometry=6x22+0+375 -e "/home/jsn/bin/markets/binance.sh -af3 -w bnbusdt" &
 
-uxterm -T 'LTC Binance' -geometry 6x25+54+340 -e '/home/jsn/bin/markets/binance.sh -as2 ltcusdt' &
+xfce4-terminal -T 'LTC Binance' --geometry=6x22+54+375 -e '/home/jsn/bin/markets/binance.sh -as2 ltcusdt' &
 
-uxterm -T 'BTC Binance US' -geometry 7x6+108+340 -e "/home/jsn/bin/markets/binance.sh -a2us btc usd" &
+xfce4-terminal -T 'BTC Binance US' --geometry=7x6+108+360 -e "/home/jsn/bin/markets/binance.sh -a2us btc usd" &
 
-uxterm -T 'BTC Bitstamp' -geometry 7x8+108+615 -e "/home/jsn/bin/markets/bitstamp.sh -c" &
+xfce4-terminal -T 'BTC Bitstamp' --geometry=7x8+108+615 -e "/home/jsn/bin/markets/bitstamp.sh -c" &
 
-uxterm -T 'BTC Bitfinex' -geometry 7x8+108+455 -e "/home/jsn/bin/markets/bitfinex.sh" &
+xfce4-terminal -T 'BTC Bitfinex' --geometry=7x6+108+485 -e "/home/jsn/bin/markets/bitfinex.sh" &
 
-uxterm -T 'XRP Binance' -geometry 7x20+0+0 -e "/home/jsn/bin/markets/binance.sh -f5 -as xrpusdt" &
+xfce4-terminal -T 'XRP Binance' --geometry=7x20+0+0 -e "/home/jsn/bin/markets/binance.sh -f5 -as xrpusdt" &
 
-uxterm -T 'BCHABC Binance' -geometry 6x20+62+0 -e "/home/jsn/bin/markets/binance.sh -aw2 bchusdt" &
+xfce4-terminal -T 'BCHABC Binance' --geometry=6x20+62+0 -e "/home/jsn/bin/markets/binance.sh -aw2 bchusdt" &
 
-uxterm -T 'ETH Binance' -geometry 6x20+115+0 -e "/home/jsn/bin/markets/binance.sh -aw2 ethusdt" &
+xfce4-terminal -T 'ETH Binance' --geometry=6x20+115+0 -e "/home/jsn/bin/markets/binance.sh -aw2 ethusdt" &
 
-uxterm -T 'BTC Detailed' -geometry 77x53+172+0 -e '/home/jsn/bin/markets/binance.sh -ai' &
+xfce4-terminal -T 'BTC Detailed' --geometry=77x53+172+0 -e '/home/jsn/bin/markets/binance.sh -ai' &
 
-uxterm -T 'BTC Book Depth Binance' -geometry 36x44+800+0 -e '/home/jsn/bin/markets/binance.sh -add' &
+xfce4-terminal -T 'BTC Book Depth Binance' --geometry=36x44+800+0 -e '/home/jsn/bin/markets/binance.sh -add' &
 
-uxterm -T 'BTC New-Block Blockchain.info' -geometry 74x17+506+495 -e '/home/jsn/bin/markets/binfo.sh -e' &
+xfce4-terminal -T 'BTC New-Block Blockchain.info' --geometry=74x17+506+495 -e '/home/jsn/bin/markets/binfo.sh -e' &
 
-uxterm -T 'Misc' -geometry 30x42+1149+149 &
+xfce4-terminal -T 'Misc' --geometry=30x42+1149+149 &
 
