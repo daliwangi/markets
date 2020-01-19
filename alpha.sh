787c52a118f4db49b9811e2b9805d3f0e4199a86
castaway <39099063+mountaineerbr@users.noreply.github.com>
2020-01-19 02:06:23 -0300

    Much improved code, new opts.

--- alpha.sh
+++ alpha.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# AlphaAvantage Stocks (Most popular Yahoo Finance API alternative)
-# v0.3-under_construction  jan/2020  by mountaineer_br
-
+# AlphaAvantage Stocks and Currency Rates (Most popular Yahoo Finance API alternative)
+# v0.3  jan/2020  by mountaineer_br
 
 # *YOUR* (free) API Private Key
 #ALPHAAPIKEY=""
@@ -13,43 +12,47 @@ DEFSTOCK=TSLA
 ## Manual and help
 ## Usage: $ alpha.sh [-opts] [symbol]
 HELP="NAME
- 	Alpha.sh -- Stocks and Index Rates from <alphavantage.co> API
+ 	Alpha.sh -- Stocks and Currency Rates from <alphavantage.co> API
 
 
 SYNOPSIS
+	alpha.sh [STOCK]
+	
+	alpha.sh [-dwm] [STOCK]
+	
+	alpha.sh -s [KEYWORDS..]
 
-	alpha.sh [-dmsw] [SYMBOL]
-
+	alpha.sh [FROM_CURRENCY] [TO_CURRENCY]
+	
+	alpha.sh [-dwm] [FROM_CURRENCY] [TO_CURRENCY]
+	
 	alpha.sh [-hlv]
 
 
 DESCRIPTION
-	This programme fetches updated rates for stocks and indexes from
-	<alphavantage.co> APIs.
+	This programme fetches updated rates for stocks, physical and digital 
+	currencies from	<alphavantage.co> APIs.
 
-	Although there are APIs for central bank and crypto currency rates from
-	<alphavantage.co>, they will not be implemented in this script.
-Claim your API Key
-https://www.alphavantage.co/support/#api-key
+	It can also return daily/weekly and monthly time series (date, open, 
+	high, low, close and volume) of the stock or currency specified, cover-
+	ing 20+ years of historical data. The most recent data point is the
+	prices and volume information of the current trading day, updated real-
+	time. 
 
-
-API Rate Limit: (5 API requests per minute; 500 API requests per day), 
-https://www.alphavantage.co/premium/
-
-#To get raw CSV data from *some* options, uncomment next line (no promisses):
-
-This API returns daily time series (date, daily open, daily high, daily low, daily close, daily volume) of the global equity specified, covering 20+ years of historical data.
-The most recent data point is the prices and volume information of the current trading day, updated realtime. 
-
-
-
-
-LIMITS
+	Required packages are JQ and Curl or Wget.
 
 
 API KEY
 	Please create a free API key and add it to the script source-code or 
 	export it as an environment variable called 'ALPHAAPIKEY'.
+
+	Claim your free API Key at <https://www.alphavantage.co/support/#api-key>.
+ 	They say they are committed to making it free forever.
+
+
+LIMITS
+	API rate limits are 5 API requests per minute or 500 API requests per 
+	day for free accounts, according to <https://www.alphavantage.co/premium/>.
 
 
 WARRANTY
@@ -62,55 +65,75 @@ WARRANTY
 
 
 USAGE EXAMPLES
+	( 1 ) 	Get AAPL stock rate:
+		
+		alpha.sh aapl
 
 
+	( 2 ) 	Get USD BRL market rate:
+		
+		alpha.sh usd brl
+
+
+	( 3 ) 	Daily time series (historic rates) for AAPL:
+		
+		alpha.sh -d aapl
 
 
 OPTIONS
-	-c 		Raw CSV data for some options (no promisses).
+	Time series
+	-d 	Daily time series (historic data).
 
-	-D 		Demo mode, only MSFT.
-
-	-d [SYMBOL] 	Daily time series (historic data).
+	-w 	Weekly time series.
 	
-	-h 		Show this help.
+	-m 	Monthly time series.
 	
-	-j 		Debug, print json.
+	Search/list symbols
+	-s 	Search for stock (stocks only) given keywords.
 
-	-m [SYMBOL]	Monthly time series.
+	-l 	List supported currencies.
 
-	-s [KEYWORDS] 	Search for symbols.
+	Other
+	-D 	Demo mode (demo API Key), only MSFT.
+	
+	-h	Show this help.
+	
+	-jj 	Debug, print raw JSON; twice to try and get CSV data instead.
 
-	-v 	  	Show this programme version.
-
-	-w [SYMBOL]	Weekly time series."
+	-v   	Show this programme version."
 
 #functions
 
-pricef() {
+stockf() {
 	#get data
 	DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${1^^}&apikey=${ALPHAAPIKEY}${CSVOPT}")"
 	
-	# Print JSON?
+	# Print raw data?
 	if [[ -n ${PJSON} ]]; then
 		printf "%s\n" "${DATA}"
 		exit 0
 	fi
 
-	# Check for error and try to search symbol
-	if jq -r 'keys[]' <<< "${DATA}" | grep -qi err; then
-		printf "Invalid symbol. Trying to search for similar symbols...\n"
-		alphas "${*}"
-		exit
+	#test for error esponse
+	if jq -e '."Error Message"' <<<"${DATA}" &>/dev/null; then
+		printf 'Error: check symbol(s).\n' 1>&2
+		exit 1
+	elif grep -qi 'standard API call frequency is 5 calls per minute and 500 calls per day' <<<"${DATA}"; then
+		printf 'Error: %s\n' "$(jq -r '.Note' <<<"${DATA}")" 1>&2
+		exit 1
 	fi
 
-	jq -r '."Global Quote" | "Symbol: \(."01. symbol")",
-	"Price: \(."05. price")",
-	"O: \(."02. open")  L: \(."04. low")  H: \(."03. high")",
-	"Vol: \(."06. volume")",
-	"Prev Close: \(."08. previous close")",
-	"Change: \(."09. change") [\(."10. change percent")]",
-	"Last Trade Day: \(."07. latest trading day")"' <<< "${DATA}"
+	jq -r '."Global Quote"|
+			"Symbol_: \(."01. symbol")",
+			"LastDay: \(."07. latest trading day")",
+			"PrevClo: \(."08. previous close")",
+			"Open___: \(."02. open")",
+			"Low____: \(."04. low")",
+			"High___: \(."03. high")",
+			"Volume_: \(."06. volume")",
+			"Change_: \(."09. change")",
+			"Change%: \(."10. change percent")",
+			"Price__: \(."05. price")"' <<< "${DATA}"
 }
 
 #search for stock symbol
@@ -121,19 +144,22 @@ searchf() {
 	#get data
 	DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${WORDS}&apikey=${ALPHAAPIKEY}${CSVOPT}")"
 	
-	# Print JSON?
+	# Print raw data?
 	if [[ -n ${PJSON} ]]; then
 		printf "%s\n" "${DATA}"
 		exit 0
 	fi
 	
-	#check for no results
+	#check for errors and process table
 	if jq -e '.bestMatches[]' <<<"${DATA}" &>/dev/null; then
 		#check if stdout is open and trim column NAME
 		[[ -t 1 ]] && TRIMCOL='-TNAME,REGION'
 		#process data and make table
 		jq -r '.bestMatches[]|
 			"\(."1. symbol"),\(."2. name"),\(."3. type"),\(."4. region"),\(."5. marketOpen"),\(."6. marketClose"),\(."7. timezone"),\(."8. currency"),\(."9. matchScore")"' <<<"${DATA}" | column -et -s',' -NSYMBOL,NAME,TYPE,REGION,MOPEN,MCLOSE,TZ,CUR,SCORE ${TRIMCOL} 
+	elif grep -qi 'standard API call frequency is 5 calls per minute and 500 calls per day' <<<"${DATA}"; then
+		printf 'Error: %s\n' "$(jq -r '.Note' <<<"${DATA}")" 1>&2
+		exit 1
 	else
 		printf 'No results -- %s\n' "${*}" 1>&2
 		exit 1
@@ -141,51 +167,121 @@ searchf() {
 }
 
 tsf() {
-	#time series
-	#get stock/index data
-	DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=TIME_SERIES_${PERIOD}&symbol=${1^^}&outputsize=full&apikey=${ALPHAAPIKEY}${CSVOPT}")"
+	if [[ -z "${2}" ]]; then
+		#time series
+		#get stock data
+		DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=TIME_SERIES_${PERIOD}&symbol=${1^^}&outputsize=full&apikey=${ALPHAAPIKEY}&datatype=csv")"
+	else
+		#get forex data
+		DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=${1^^}&to_symbol=${2^^}&outputsize=full&apikey=${ALPHAAPIKEY}&datatype=csv")"
+	fi
 
-	# Print JSON?
+	# Print raw data?
 	if [[ -n ${PJSON} ]]; then
 		printf "%s\n" "${DATA}"
+		[[ -z "${CSVOPT}" ]] && printf 'Using CSV data for this call.\n' 1>&2
 		exit 0
 	fi
+
+	#test for error esponse
+	if jq -e '."Error Message"' <<<"${DATA}" &>/dev/null; then
+		printf 'Error: check symbol(s).\n' 1>&2
+		exit 1
+	elif grep -qi 'standard API call frequency is 5 calls per minute and 500 calls per day' <<<"${DATA}"; then
+		printf 'Error: %s\n' "$(jq -r '.Note' <<<"${DATA}")" 1>&2
+		exit 1
+	fi
+
+	#get number of records
+	NREC="$(sed 1d <<<"${DATA}" | wc -l)"
+	#get col names
+	COLNAMES="$(sed -n 1p <<<"${DATA}")"
 	
+	#make table
+	#cols: timestamp,open,high,low,close,volume
+	sed 1d <<<"${DATA}" | column -et -s, -N"${COLNAMES^^}"
+	printf 'Symbol_: %s %s\n' "${1^^}" "${2^^}"
+	printf 'Records: %s\n' "${NREC}"
 
-
-	#get forex data
-	DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=${1^^}&to_symbol=${2^^}&outputsize=full&apikey=${ALPHAAPIKEY}${CSVOPT}")"
-
-
+	#jq -r '(."Meta Data"|
+	#		"\(."1. Information")",
+      	#		"\(."2. Symbol")",
+      	#		"\(."3. Last Refreshed"),
+      	#		"\(."4. Output Size"),
+      	#		"\(."5. Time Zone")
+	#	),
+	#	(."Time Series (Daily)")'
 }
 
 forexf() {
-	#get currency rate
-	#get data
-	DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${1^^}&to_currency=${2^^}&apikey=${ALPHAAPIKEY}${CSVOPT}")"
+	#currency rate
 	
+	#get data
+	DATA="$(${YOURAPP} "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${1^^}&to_currency=${2^^}&apikey=${ALPHAAPIKEY}")"
+
+	# Print raw data?
+	if [[ -n ${PJSON} ]]; then
+		printf "%s\n" "${DATA}"
+		[[ -n "${CSVOPT}" ]] && printf 'No CSV data for this call.\n' 1>&2
+		exit 0
+	fi
+
+	#test for error esponse
+	if jq -e '."Error Message"' <<<"${DATA}" &>/dev/null; then
+		printf 'Error: check symbol(s).\n' 1>&2
+		exit 1
+	elif grep -qi 'standard API call frequency is 5 calls per minute and 500 calls per day' <<<"${DATA}"; then
+		printf 'Error: %s\n' "$(jq -r '.Note' <<<"${DATA}")" 1>&2
+		exit 1
+	fi
+
+	jq -r '."Realtime Currency Exchange Rate"|
+		"Realtime Currency Exchange Rate",
+		"Updated: \(."6. Last Refreshed") \(."7. Time Zone")",
+		"FromCod: \(."1. From_Currency Code")",
+		"FromNam: \(."2. From_Currency Name")",
+		"To_Code: \(."3. To_Currency Code")",
+		"To_Name: \(."4. To_Currency Name")",
+        	"Bid____: \(."8. Bid Price")",
+		"Ask____: \(."9. Ask Price")",
+		"Rate___: \(."5. Exchange Rate")"' <<<"${DATA}"
 }
 
 lforexf() {
 	#list currencies
 	
-	#get data
-	#physical currency  #csv file
-	DATA="$(${YOURAPP} "https://www.alphavantage.co/physical_currency_list/")"
+	# Print raw data?
+	if [[ -n ${PJSON} ]]; then
+		${YOURAPP} "https://www.alphavantage.co/digital_currency_list/"
+		printf '\n'
+		${YOURAPP} "https://www.alphavantage.co/physical_currency_list/"
+		exit 0
+	fi
 
+	#get data and process
+	
 	#digital/crypto currency
-	DATA2="$(${YOURAPP} "https://www.alphavantage.co/digital_currency_list/")"
+	DATA="$(${YOURAPP} "https://www.alphavantage.co/digital_currency_list/" | sed 1d)"
+	NP="$(wc -l <<<"${DATA}")" 
+	
+	#digital currencies
+	#cols: currency code,currency name
+	printf 'Digital currencies:\n'
+	column -et -s',' -NCODE,NAME <<<"${DATA}"
+	
+	#physical currency  #csv file
+	DATA="$(${YOURAPP} "https://www.alphavantage.co/physical_currency_list/" | sed 1d)"
+	ND="$(wc -l <<<"${DATA}")" 
+	printf 'Physical currencies:\n'
+	column -et -s',' -NCODE,NAME <<<"${DATA}"
 
+	printf 'Digital_: %s\n' "${ND}"
+	printf 'Physical: %s\n' "${NP}"
 }
 
-
 # Parse options
-while getopts ":cDdhjmsvw" opt; do
+while getopts ":Ddhjlmsvw" opt; do
 	case ${opt} in
-		( c ) #CSV data
-			CSVOPT='&datatype=csv'
-			PJSON=1
-			;;
 		( D ) #demo mode
 			ALPHAAPIKEY=demo
 			;;
@@ -196,8 +292,11 @@ while getopts ":cDdhjmsvw" opt; do
 			printf '%s\n' "${HELP}"
 			exit 0
 			;;
-		( j ) # Print JSON
-			PJSON=1
+		( j ) # Print raw data
+			[[ -z "${PJSON}" ]] && PJSON=1 || CSVOPT='&datatype=csv'
+			;;
+		( l ) #list currencies
+			LFOREXOPT=1
 			;;
 		( m ) #montly time series
 			PERIOD=MONTHLY
@@ -228,7 +327,7 @@ if [[ -z "${ALPHAAPIKEY}" ]]; then
 fi
 
 # Test for must have packages
-if ! command -v jq &>/dev/null && [[ -z "${CSVOPT}" ]]; then
+if ! command -v jq &>/dev/null && [[ -z "${PJSON}${LFOREXOPT}${PERIOD}" ]]; then
 	printf "JQ is required.\n" 1>&2
 	exit 1
 fi
@@ -248,10 +347,14 @@ fi
 
 #call opt functions
 if [[ -n "${SOPT}" ]]; then
-	searchf "${*}"
+	searchf "${@}"
+elif [[ -n "${LFOREXOPT}" ]]; then
+	lforexf
 elif [[ -n "${PERIOD}" ]]; then
-	tsf "${*}"
+	tsf "${@}"
+elif [[ -z "${2}" ]]; then
+	stockf "${@}"
 else
-	pricef "${*}"
+	forexf "${@}"
 fi
 
