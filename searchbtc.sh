0b26180f3ed3fe41887c2eaae2a33a022ecc0e71
castaway <39099063+mountaineerbr@users.noreply.github.com>
2020-02-15 12:43:10 -0300

    Add files via upload

--- searchbtc.sh
+++ searchbtc.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# v0.2.33  13/nov/2019
+# v0.2.36  feb/2020
 
-# You can create a blockchair.com API key for more requests/min
+#if you have got a BlockChair api key for higher limit:
 #CHAIRKEY="?key=MYSECRETKEY"
 
+# DEFAULTS
+# Pay attention to rate limits
+#time between new queries
+SLEEPTIME=10
+
+#log file
+RECFILE="${HOME}/addresses.log"
+
+#curl/wget timeout to estabilish connection
+TIMEOUT=20
+
 # Help -- run with -h
-HELP="WARRANTY
-	Licensed under the GNU Public License v3 or better.
- 	This programme is distributed without support or bug corrections.
+HELP="SYNOPSIS
 
-
-SYNOPSIS
-
-	searchbtc.sh [-abcdghv] [-sNUM] [-o\"FILE_PATH\"]
+	${0} [-abcdghv] [-sNUM] [-o\"FILE_PATH\"]
 
 
 	This script uses Vanitygen to generate an address and its private key.
@@ -20,7 +26,8 @@ SYNOPSIS
 	dress. If a transaction is detected, even if the balance is currently 
 	zero (current balance is not checked), a copy of the generated private 
 	key and its public address will be printed in the screen and logged 
-	to ~/ADDRESS. You can change record file path with option \"-o\".
+	to ${RECFILE}.
+	You can change the log base name and path with option \"-o\".
 
 	The fastest way of brute forcing a bitcoin address collision is to have 
 	your own full-node set up. However, that may not be feasible size-wise.
@@ -52,14 +59,14 @@ COLLISION PROBABILITIES
 	addresses, you ought not to keep the bitcoins.
 
 
-	A nice page about cryptographic hash functions is (3).
-	Check those jokes!
+	A nice page about cryptographic hash functions is (3). Check those jokes!
 
 
-References:
-	(1) <https://www.youtube.com/watch?v=fOMVZXLjKYo>
-	(2) <https://www.blockchain.com/en/charts/n-unique-addresses>
-	(3) <http://valerieaurora.org/hash.html>
+	References:
+		(1) <https://www.youtube.com/watch?v=fOMVZXLjKYo>
+		(2) <https://www.blockchain.com/en/charts/n-unique-addresses>
+		(3) <http://valerieaurora.org/hash.html>
+
 
 RATE LIMITS
 	Blockchain.info, from Twitter 2013:
@@ -97,6 +104,11 @@ RATE LIMITS
 	Error 429 may not be a problem. Perhaps error 430 is.
 
 
+WARRANTY
+	Licensed under the GNU Public License v3 or better.
+ 	This programme is distributed without support or bug corrections.
+
+
 USAGE EXAMPLES
 	(1) Use defaults, sleep time between queries is nought and check
 	    response from server.
@@ -118,8 +130,8 @@ USAGE EXAMPLES
 OPTIONS
 	-a 		Use BTC.com API.
 	
-	-b 		Use Blockchain.info APIs (defaults if no 
-			server opt is given).
+	-b 		Use Blockchain.info APIs; defaults if no server opt is
+			given.
 
 	-c 		Use Blockchair.com API.
 	
@@ -129,94 +141,23 @@ OPTIONS
 
 	-h 		Show this help.
 
-	-o [FILE_PATH] 	File path to record positive match results;
-			defaults=\"~/ADDRESS\"
+	-o [FILE_PATH] 	File path to record positive match results; 
+			defaults=\"${RECFILE}\"
 
-	-s [NUM] 	Sleep time (seconds) between new queries; 
-			reccomended>=2; defaults=10.
+	-s [NUM] 	Sleep time (seconds) between new queries; value of zero
+			is possible, although api servers may block you quickly;
+			defaults=${SLEEPTIME}.
 
 	-v 		Print script version."
 
-# DEFAULTS
-# Pay attention to rate limits
-SLEEPTIME="10"
-RECFILE="${HOME}/ADDRESS"
-TIMEOUT="6"
-
-# Must have vanitygen
-if ! command -v vanitygen >/dev/null; then
-	printf "Vanitygen is required.\n" 1>&2
-	exit 1
-fi
-# Must have cURL or Wget
-if command -v curl >/dev/null; then
-	MYAPP="curl -s --retry 1 -m${TIMEOUT}"
-elif command -v wget >dev/null; then
-	MYAPP="wget  -t1 -T${TIMEOUT} -qO-"
-else
-	printf "cURL or Wget is required.\n" 1>&2
-	exit 1
-fi
-if ! command -v jq >/dev/null; then
-	printf "JQ is required.\n" 1>&2
-	exit 1
-fi
-
-# Parse options
-while getopts ":cbadghs:vo:" opt; do
-	case ${opt} in
-		a ) # Use BTC.com
-			BTCOPT=1
-			SERVERSET=1
-			;;
-		b ) # Use Blockchain.info
-			BINFOOPT=1
-			SERVERSET=1
-			;;
-		c ) # Use Blockchair.com
-			CHAIROPT=1
-			SERVERSET=1
-			;;
-		d ) # Use Blockcypher.com
-			CYPHEROPT=1
-			SERVERSET=1
-			;;
-		h ) # Help
-			head "${0}" | grep -e '# v'
-			echo -e "${HELP}"
-			exit 0
-			;;
-		o ) # Record file path
-			RECFILE="${OPTARG}"
-			;;
-		v ) # Version of Script
-			head "${0}" | grep -e '# v'
-			exit 0
-			;;
-		s ) # Sleep time
-			SLEEPTIME="${OPTARG}"
-			;;
-		g ) # Debug
-			DEBUG=1
-			;;
-		\? )
-			echo "Invalid Option: -$OPTARG" 1>&2
-			exit 1
-			;;
-	 esac
-done
-shift $((OPTIND -1))
-# Use only Blockchain.com by defaults
-if [[ -z "${SERVERSET}" ]]; then
-	BINFOOPT=1
-fi
+#functions
 
 #Which function
 whichf() {
-	test "${PASS}" = "1" && printf "Blockchain.info\n" 
-	test "${PASS}" = "2" && printf "Blockchair.com\n"
-	test "${PASS}" = "3" && printf "BTC.com\n"
-	test "${PASS}" = "4" && printf "Blockcypher.com\n"
+	[[ "${PASS}" = 1 ]] && printf "Blockchain.info\n" 
+	[[ "${PASS}" = 2 ]] && printf "Blockchair.com\n"
+	[[ "${PASS}" = 3 ]] && printf "BTC.com\n"
+	[[ "${PASS}" = 4 ]] && printf "Blockcypher.com\n"
 	#test "${PASS}" = "" && printf " "
 }
 
@@ -248,11 +189,10 @@ queryf() {
 }
 
 #Get RECEIVED TOTAL (not really balance)
-SA=0
 getbal() {
 	# Test for rate limit error
 	if grep -iq -e "Please try again shortly" -e "Quota exceeded" -e "Servlet Limit" -e "rate limit" -e "exceeded" -e "limited" -e "not found" -e "429 Too Many Requests" -e "Error 402" -e "Error 429" -e "too many requests" -e "banned" -e "Maximum concurrent requests" -e "Please try again shor" -e "\"error\":" -e "upgrade your plan" -e "extend your limits" <<< "${QUERY}"; then
-		SA="$((SA+1))"
+		((SA++))
 		printf "\nRate limit warning/error: %s.\n" "$(whichf)" 1>&2
 		printf "Skipped: %s\n" "${SA}" 1>&2
 		#Debug Verbose
@@ -264,14 +204,14 @@ getbal() {
 			printf "\n.............." 1>&2
 		fi
 		#continue...
-	elif grep -iq -e "Invalid API token" -e "invalid api" -e "wrong api" -e "wrong key" -e "api key" <<< "${QUERY}"; then
+	elif grep -iq -e "Invalid API token" -e "invalid api" -e "wrong api" -e "wrong key" <<< "${QUERY}"; then
 		printf "\nInvalid API token.\n" 1>&2
 		exit 1
 	fi
 	# Choose processing between 
 	if [[ "${PASS}" -eq "1" ]]; then
 		# Binfo.com
-		jq -er '.["'"${address}"'"].total_received' <<< "${QUERY}" 2>/dev/null || return 1
+		jq -er '.[].total_received' <<< "${QUERY}" 2>/dev/null || return 1
 		return 0
 	elif [[ "${PASS}" -eq "2" ]]; then
 		# Blockchair.com
@@ -288,38 +228,104 @@ getbal() {
 		jq -er '.total_received' <<< "${QUERY}" 2>/dev/null || return 1
 		return 0
 	fi
-	}
+}
+
+#parse opt
+
+# Parse options
+while getopts ":cbadghs:vo:" opt; do
+	case ${opt} in
+		a ) # Use BTC.com
+			BTCOPT=1
+			;;
+		b ) # Use Blockchain.info
+			BINFOOPT=1
+			;;
+		c ) # Use Blockchair.com
+			CHAIROPT=1
+			;;
+		d ) # Use Blockcypher.com
+			CYPHEROPT=1
+			;;
+		h ) # Help
+			head "${0}" | grep -e '# v'
+			echo -e "${HELP}"
+			exit 0
+			;;
+		o ) # Record file path
+			RECFILE="${OPTARG}"
+			;;
+		v ) # Version of Script
+			head "${0}" | grep -e '# v'
+			exit 0
+			;;
+		s ) # Sleep time
+			SLEEPTIME="${OPTARG}"
+			;;
+		g ) # Debug
+			DEBUG=1
+			;;
+		\? )
+			echo "Invalid Option: -$OPTARG" 1>&2
+			exit 1
+			;;
+	 esac
+done
+shift $((OPTIND -1))
+
+# Use only Blockchain.com by defaults
+if [[ -z "${BTCOPT}${BINFOOPT}${CHAIROPT}${CYPHEROPT}" ]]; then
+	BINFOOPT=1
+fi
+
+# Must have vanitygen
+if ! command -v vanitygen >/dev/null; then
+	printf "Vanitygen is required.\n" 1>&2
+	exit 1
+fi
+# Must have cURL or Wget
+if command -v curl >/dev/null; then
+	MYAPP="curl -sLb non-existing --retry 1 --connect-timeout ${TIMEOUT}"
+elif command -v wget >dev/null; then
+	MYAPP="wget  -t1 -T${TIMEOUT} -qO-"
+else
+	printf "cURL or Wget is required.\n" 1>&2
+	exit 1
+fi
+if ! command -v jq >/dev/null; then
+	printf "JQ is required.\n" 1>&2
+	exit 1
+fi
 
 # Heading
 date 
 # Loop
 # Start count
-N=1
 while :; do
+	((N++))
 	printf "\rAddrs: %07d" "${N}" 1>&2
 	VANITY="$(vanitygen -q 1)"
+	printf '\n%s\n' "${VANITY}" >> "${RECFILE}.all"
 	address="$(grep -e "Address:" <<< "${VANITY}" | cut -d' ' -f2)"
 	queryf
-	# If JQ detects an error, skip address
-	if ! getbal >/dev/null; then
-		sleep 15
+	
+	# If JQ detects an error, skip address and sleep
+	if ! REC="$(getbal)" >/dev/null; then
+		sleep 300
 		continue
 	fi
+
 	# Get received amount for further processing
-	REC="$(getbal)"
 	if [[ -n "${REC}" ]] && [[ "${REC}" != "0" ]] && [[ "${REC}" != "null" ]] ; then
 		{ date
 		  printf 'Check this address! \n'
 		  printf "%s\n" "${VANITY}"
 		  printf "Received? %s\n" "${REC}"
 		  printf "Addrs checked: %s.\n" "${N}"
-		} | tee -a "${RECFILE}"
+		} | tee -a "${RECFILE}" "${RECFILE}.all"
 	fi
 	sleep "${SLEEPTIME}"
-	N=$((N+1))
 done
-
-exit
 
 #Dead code
 
