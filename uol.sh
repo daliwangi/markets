10872e1303e5a392577b01c44f93fa6ae0e05815
castaway <39099063+mountaineerbr@users.noreply.github.com>
2020-02-19 14:59:40 -0300

    adicionadas series históricas e intraday
    para dolar e bolsa b3.
    mudei opções.

--- uol.sh
+++ uol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Uol.sh -- Puxa cotações do portal do UOL
-# v0.1.20  feb/2020  by mountaineer_br
+# v0.1.30  feb/2020  by mountaineer_br
 
 AJUDA="Uol.sh -- Puxa dados do UOL Economia
 
@@ -31,11 +31,17 @@ GARANTIA
 
 
 OPÇÕES
-	-b 	Índice da B3, série intraday com a opção '-i'.
-
-	-d 	Cotação do dólar, série intraday com a opção '-i'.
+	-b 	Índice da B3.
 	
-	-i 	Série intraday com as opções '-b' e '-d'.
+	-bb 	Série B3 intraday.
+	
+	-bbb 	Série B3 histórica.
+	
+	-d 	Cotação do dólar turismo e comercial, ticker simples.
+	
+	-dd 	Séries dólares intraday.
+	
+	-ddd 	Séries dólares históricas.
 	
 	-h 	Mostra esta ajuda.
 
@@ -53,79 +59,104 @@ hf() {  sed 's/<[^>]*>//g';}
 
 # Cotação da BOVESPA B3 Hack
 b3f() {
-	UOLB3="$(${YOURAPP} 'https://api.cotacoes.uol.com/asset/intraday/list/?format=JSON&fields=price,high,low,open,volume,close,change,pctChange,date&item=1&://api.cotacoes.uol.com/asset/intraday/list/?format=JSON&fields=price,high,low,open,volume,close,bid,ask,change,pctChange,date&item=1&')"
-	#bid,ask,
+	if ((B3OPT<=2)); then
+		#get data
+		UOLB3="$(${YOURAPP} 'https://api.cotacoes.uol.com/asset/intraday/list/?format=JSON&fields=price,high,low,open,volume,close,change,pctChange,date&item=1&')"
+		#bid,ask,
 
-	#Debug?
-	if [[ -n "${PJSON}" ]]; then
-		printf "%s\n" "${UOLB3}"
-		exit
-	fi
-	
-	if [[ -z "${INTRADOPT}" ]]; then
-		printf 'UOL - Bovespa B3\n'
-		jq -r '.docs[0]|
-			"Data   : \(.date)",
-			"Alta   : \(.high)",
-			"Baixa  : \(.low)",
-			"Abertur: \(.open)",
-			"Fechame: \(.close)",
-			"Var    : \(.change)",
-			"Var%   : \(.pctChange)%",
-			"Pontos : \(.price)"'<<<"${UOLB3}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/'
-	else
-		printf 'UOL - Bovespa B3\n'
-		jq -r '.docs|reverse[]|"\(.price)\t\(.high)\t\(.low)\t\(.open)\t\(.close)\t\(.change)\t\(.pctChange)\t\(.date)"'<<<"${UOLB3}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/' | column -ets$'\t' -NPREÇO,ALTA,BAIXA,ABERT,FECHA,VAR,VAR%,DATA
-
-	fi
+		#Debug?
+		if [[ -n "${PJSON}" ]]; then
+			printf "%s\n" "${UOLB3}"
+			exit
+		fi
 		
+		#simple tickers
+		if [[ "${B3OPT}" = 1 ]]; then
+			printf 'UOL - Bovespa B3\n'
+			jq -r '.docs[0]|
+				"Data   : \(.date)",
+				"Alta   : \(.high)",
+				"Baixa  : \(.low)",
+				"Abertur: \(.open)",
+				"Fechame: \(.close)",
+				"Var    : \(.change)",
+				"Var%   : \(.pctChange)%",
+				"Pontos : \(.price)"'<<<"${UOLB3}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/'
+		#série intraday
+		else
+			printf 'UOL - Bovespa B3\n'
+			jq -r '.docs|reverse[]|"\(.price)\t\(.high)\t\(.low)\t\(.open)\t\(.close)\t\(.change)\t\(.pctChange)%\t\(.date)"'<<<"${UOLB3}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/' | column -ets$'\t' -NPONTOS,ALTA,BAIXA,ABERT,FECHA,VAR,VAR%,DATA
+		fi
+	#serie histórica ano
+	else
+		UOLB3="$(${YOURAPP} 'https://api.cotacoes.uol.com/asset/interday/list/years/?format=JSON&fields=price,high,low,open,volume,close,bid,ask,change,pctChange,date&item=1&')"
+		#Debug?
+		if [[ -n "${PJSON}" ]]; then
+			printf "%s\n" "${UOLB3}"
+			exit
+		fi
+		
+		printf 'UOL - Bovespa B3\n'
+		jq -r '.docs|reverse[]|"\(.price)\t\(.high)\t\(.low)\t\(.open)\t\(.close)\t\(.change)\t\(.pctChange)%\t\(.date)"'<<<"${UOLB3}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/' | column -ets$'\t' -NPONTOS,ALTA,BAIXA,ABERT,FECHA,VAR,VAR%,DATA
+	fi
 }
 
 #Cotação dólar
 dolarf() {
-	#get data
-	DT="$(${YOURAPP} 'https://api.cotacoes.uol.com/currency/intraday/list/?format=JSON&fields=bidvalue,askvalue,maxbid,minbid,variationbid,variationpercentbid,date&currency=3&')"
-	DC="$(${YOURAPP} 'http://cotacoes.economia.uol.com.br/cambioJSONChart.html')"
+	#subfunction
+	tablef() { jq -r '.docs|reverse[]|"\(.bidvalue)\t\(.askvalue)\t\(.maxbid)\t\(.minbid)\t\(.variationbid)\t\(.variationpercentbid)%\t\(.date)"' | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/' | column -ets$'\t' -NCOMPRA,VENDA,MAX,MIN,VAR,VAR%,DATA -TDATA;}
 
-	#Debug?
-	if [[ -n "${PJSON}" ]]; then
-		printf "%s\n" "${DT}"
-		printf "%s\n" "${DC}"
-		exit
-	fi
-	
-	if [[ -z "${INTRADOPT}" ]]; then
-		#turismo
-		jq -r '.docs[0]|
-			"UOL - Dólar Turismo",
-			"Data    : \(.date)",
-			"VarCompr: \(.variationbid)",
-			"VarComp%: \(.variationpercentbid)",
-			"Venda   : \(.askvalue)",
-			"Compra  : \(.bidvalue)"' <<< "${DT}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/'
-		
-		#comercial
-		jq -r '.[2]|
-			"",
-			"UOL - \(.name)",
-			"Data    : \(.timestamp/1000|strflocaltime("%Y-%m-%dT%H:%M:%S%Z"))",
-			"Fresco  : \(if .notFresh == true then "não" else "sim" end)",
-			"Abertura: \(if .open == "0" then empty else .open end)",
-			"Alta    : \(.high)",
-			"Baixa   : \(.low)",
-			"Var(%)  : \(.pctChange)",
-			"Venda   : \(.ask)",
-			"Compra  : \(.bid)",
-			"VarComp.: \(.varBid)"' <<< "${DC}"
-			#TS="$(jq -r '.[2].timestamp' <<<"${COT}")"
-			#printf "Hora    :%s" "$(date -d@"${TS:0:10}")"
-	else
-		#subfunction
-		tablef() { sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/' | column -ets$'\t' -NCOMPRA,VENDA,VAR,VAR%,DATA;}
-
-		#get data	
+	if [[ "${DOLAROPT}" =~ 1|2 ]]; then
+		#get data
 		DT="$(${YOURAPP} 'https://api.cotacoes.uol.com/currency/intraday/list/?format=JSON&fields=bidvalue,askvalue,maxbid,minbid,variationbid,variationpercentbid,date&currency=3&')"
 		DC="$(${YOURAPP} 'https://api.cotacoes.uol.com/currency/intraday/list/?format=JSON&fields=bidvalue,askvalue,maxbid,minbid,variationbid,variationpercentbid,date&currency=1&')"
+		#DC="$(${YOURAPP} 'http://cotacoes.economia.uol.com.br/cambioJSONChart.html')"
+
+		#Debug?
+		if [[ -n "${PJSON}" ]]; then
+			#printf "%s\n" "${DT}"
+			printf "%s\n" "${DC}"
+			exit
+		fi
+
+		#simple tickers
+		if [[ "${DOLAROPT}" = 1 ]]; then
+			#turismo
+			jq -r '.docs[0]|
+				"UOL - Dólar Turismo",
+				"Data____: \(.date)",
+				"VarCompr: \(.variationbid)",
+				"Var%Comp: \(.variationpercentbid)%",
+				"MaxCompr: \(.maxbid)",
+				"MinCompr: \(.minbid)",
+				"Venda___: \(.askvalue)",
+				"Compra__: \(.bidvalue)"' <<< "${DT}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/'
+
+			#comercial
+			jq -r '.docs[0]|
+				"",
+				"UOL - Dólar Comercial",
+				"Data____: \(.date)",
+				"VarCompr: \(.variationbid)",
+				"Var%Comp: \(.variationpercentbid)%",
+				"MaxCompr: \(.maxbid)",
+				"MinCompr: \(.minbid)",
+				"Venda___: \(.askvalue)",
+				"Compra__: \(.bidvalue)"' <<< "${DC}" | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5:\6/'
+
+		#intraday
+		else
+			printf 'UOL - Dólar Turismo\n'
+				tablef <<< "${DT}"
+		
+			printf '\nUOL - Dólar Comercial\n'
+				tablef <<< "${DC}"
+		fi
+	#historical prices
+	else
+		#get data	
+		DT="$(${YOURAPP} 'https://api.cotacoes.uol.com/currency/interday/list/years/?format=JSON&fields=bidvalue,askvalue,maxbid,minbid,variationbid,variationpercentbid,date&currency=3&')"
+		DC="$(${YOURAPP} 'https://api.cotacoes.uol.com/currency/interday/list/years/?format=JSON&fields=bidvalue,askvalue,maxbid,minbid,variationbid,variationpercentbid,date&currency=1&')"
 
 		#Debug?
 		if [[ -n "${PJSON}" ]]; then
@@ -135,12 +166,10 @@ dolarf() {
 		fi
 
 		printf 'UOL - Dólar Turismo\n'
-		jq -r '.docs|reverse[]|
-			"\(.bidvalue)\t\(.askvalue)\t\(.variationbid)\t\(.variationpercentbid)\t\(.date)"' <<< "${DT}" | tablef
-		
+		tablef <<< "${DT}"
+	
 		printf '\nUOL - Dólar Comercial\n'
-		jq -r '.docs|reverse[]|
-			"\(.bidvalue)\t\(.askvalue)\t\(.variationbid)\t\(.variationpercentbid)\t\(.date)"' <<< "${DC}" | tablef
+		tablef <<< "${DC}"
 	fi
 
 }
@@ -195,16 +224,21 @@ fi
 
 
 # Parse options
-while getopts ':bdijlmhv' opt; do
+while getopts ':bdjlmhv' opt; do
 	case ${opt} in
 		b ) #b3 
-			B3OPT=1
+			if [[ "${B3OPT}" = 2 ]]; then
+				B3OPT=3
+			else
+				[[ -z "${B3OPT}" ]] && B3OPT=1 || B3OPT=2
+			fi
 	      		;;
 		d ) #dolar 
-			DOLAROPT=1
-	      		;;
-		i ) #timeseries intraday 
-			INTRADOPT=1
+			if [[ "${DOLAROPT}" = 2 ]]; then
+				DOLAROPT=3
+			else
+				[[ -z "${DOLAROPT}" ]] && DOLAROPT=1 || DOLAROPT=2
+			fi
 	      		;;
 		j ) #debug, print json
 			PJSON=1
